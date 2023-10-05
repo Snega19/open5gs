814b7a14927ea898a641246ef68ea67060ff5c6c
Snega19 <snegamuniyandi1900@gmail.com>
2023-10-05 17:15:52 +0530

    final3

--- main.tf
+++ main.tf
@@ -336,6 +336,9 @@ resource "null_resource" "Core-null-res" {
     inline = [
       "cloud-init status --wait",
       file("${path.module}/microk8s.sh"),
+      "#!/bin/bash",
+      "kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.22/deploy/local-path-storage.yaml",
+      "kubectl patch storageclass local-path -p '{\"metadata\": {\"annotations\":{\"storageclass.kubernetes.io/is-default-class\":\"true\"}}}'",
       file("${path.module}/core.sh")
     ]
   }
