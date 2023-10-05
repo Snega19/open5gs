d750c9981acf8e4d6a2e96ad800e728cbae6e5f6
Snega19 <snegamuniyandi1900@gmail.com>
2023-10-05 15:25:21 +0530

    final2

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.0.0" # Replace with the desired AWS provider version
+      version = "3.0.0" 
     }
   }
 }
