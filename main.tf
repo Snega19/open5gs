aed5f28af6233ab6bd07f41155d06fc99e21a765
Snega <117353362+Snega19@users.noreply.github.com>
2023-10-11 17:24:32 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,17 @@
 terraform {
-  required_version = ">=0.13.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.0.0" 
+      version = "~> 4.18.0"
     }
+  }
+
+  backend "s3" {
+    bucket         	   = "the-bucketforgithubaction"
+    key              	   = "state/terraform.tfstate"
+    region         	   = "us-east-1"
+    encrypt        	   = true
+    dynamodb_table = "the-bucketforgithubaction-dmtbl"
   }
 }
 
